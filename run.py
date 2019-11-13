02d7bda0eed3897624a9ce73769b86f11c769cd0
Ben Jefferies <benjefferies@echosoft.uk>
2019-11-13 14:11:14 +0000

    Retry mechanism with exponential back-off

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import os
+from time import sleep
 
 from github3 import login
 import configargparse
-from github3.exceptions import NotFoundError
+from github3.exceptions import NotFoundError, GitHubException
 
 
-def toggle_enforce_admin(access_token, owner, repo_name, branch_name):
+def toggle_enforce_admin(access_token, owner, repo_name, branch_name, retries):
     # or using an access token
     gh = login(token=access_token)
     try:
@@ -15,12 +16,22 @@ def toggle_enforce_admin(access_token, owner, repo_name, branch_name):
         raise
     branch = repo.branch(branch_name)
     protection = branch.protection()
-    print(f"Is admin branch protection enabled? {protection.enforce_admins.enabled}")
-    print(f"Setting admin branch protection enabled to {not protection.enforce_admins.enabled}")
-    if protection.enforce_admins.enabled:
-        protection.enforce_admins.disable()
-    else:
-        protection.enforce_admins.enable()
+    print(f"Enforce admins branch protection enabled? {protection.enforce_admins.enabled}")
+    print(f"Setting enforce admins branch protection to {not protection.enforce_admins.enabled}")
+    for i in range(retries):
+        try:
+            if protection.enforce_admins.enabled:
+                protection.enforce_admins.disable()
+                return
+            else:
+                protection.enforce_admins.enable()
+                return
+        except GitHubException:
+            print(f"Failed to set enforce admins to {not protection.enforce_admins.enabled}. Retrying...")
+            sleep(i ** 2)  # Exponential back-off
+
+    print(f"Failed to set enforce admins to {not protection.enforce_admins.enabled}.")
+    exit(1)
 
 
 if __name__ == '__main__':
@@ -29,6 +40,7 @@ if __name__ == '__main__':
     p.add_argument('-o', '--owner', env_var='OWNER', required=True, help='Owner. For example benjefferies for https://github.com/benjefferies/branch-protection-bot')
     p.add_argument('-r', '--repo', env_var='REPO', required=True, help='Repo. For example branch-protection-bot for https://github.com/benjefferies/branch-protection-bot')
     p.add_argument('-b', '--branch', env_var='BRANCH', default='master', help='Branch name')
+    p.add_argument('--retries', env_var='RETRIES', default=5, help='Number of times to retry before exiting')
 
     options = p.parse_args()
-    toggle_enforce_admin(options.access_token, options.owner, options.repo, options.branch)
+    toggle_enforce_admin(options.access_token, options.owner, options.repo, options.branch, options.retries)
