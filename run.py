f72de453736fd435d60587714e05d197d962fd7a
Ben Jefferies <benjefferies@echosoft.uk>
2019-12-09 16:08:18 +0000

    Accept GITHUB_REPOSITORY as a default environment variable from (#4)
    * Accept GITHUB_REPOSITORY as a default environment variable from
    https://help.github.com/en/actions/automating-your-workflow-with-github-actions/using-environment-variables#default-environment-variables

--- run.py
+++ run.py
@@ -8,9 +8,17 @@ from github3.exceptions import NotFoundError, GitHubException
 
 
 def toggle_enforce_admin(options):
-    access_token, owner, repo_name, branch_name, retries = options.access_token, options.owner, options.repo, options.branch, int(options.retries)
+    access_token, owner, repo_name, branch_name, retries, github_repository = options.access_token, options.owner, options.repo, options.branch, int(options.retries), options.github_repository
+    if github_repository is not None and "/" in github_repository:
+        owner = github_repository.split("/")[0]
+        repo_name = github_repository.split("/")[1]
+
+    if owner == '' or repo_name == '':
+        print('Owner and repo or GITHUB_REPOSITORY not set')
+        raise RuntimeError
     enforce_admins = bool(strtobool(options.enforce_admins)) if options.enforce_admins is not None and not options.enforce_admins == '' else None
     # or using an access token
+    print(f"Getting branch protection settings for {owner}/{repo_name}")
     protection = get_protection(access_token, branch_name, owner, repo_name)
     print(f"Enforce admins branch protection enabled? {protection.enforce_admins.enabled}")
     print(f"Setting enforce admins branch protection to {enforce_admins if enforce_admins is not None else not protection.enforce_admins.enabled}")
@@ -63,8 +71,9 @@ def disable(protection):
 if __name__ == '__main__':
     p = configargparse.ArgParser()
     p.add_argument('-t', '--access-token', env_var='ACCESS_TOKEN', required=True, help='Github access token. https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line')
-    p.add_argument('-o', '--owner', env_var='OWNER', required=True, help='Owner. For example benjefferies for https://github.com/benjefferies/branch-protection-bot')
-    p.add_argument('-r', '--repo', env_var='REPO', required=True, help='Repo. For example branch-protection-bot for https://github.com/benjefferies/branch-protection-bot')
+    p.add_argument('-o', '--owner', env_var='OWNER', required=False, default='', help='Owner. For example benjefferies for https://github.com/benjefferies/branch-protection-bot')
+    p.add_argument('-r', '--repo', env_var='REPO', required=False, default='', help='Repo. For example branch-protection-bot for https://github.com/benjefferies/branch-protection-bot')
+    p.add_argument('--github_repository', env_var='GITHUB_REPOSITORY', required=False, default='', help='Owner and repo. For example benjefferies/branch-protection-bot for https://github.com/benjefferies/branch-protection-bot')
     p.add_argument('-b', '--branch', env_var='BRANCH', default='master', help='Branch name')
     p.add_argument('--retries', env_var='RETRIES', default=5, help='Number of times to retry before exiting')
     p.add_argument('--enforce_admins', env_var='ENFORCE_ADMINS', default=None, help='Flag to explicitly enable or disable "Include administrators"')
