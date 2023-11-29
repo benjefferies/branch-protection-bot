dd737e623644c41e6c8185f6b44fcfaebc404323
Ben Jefferies <benjefferies@echosoft.uk>
2023-11-29 23:46:08 +0000

    chore(python): upgrade Python version from 3.7 to 3.12 in GitHub actions, .tool-versions, and Pipfile to leverage latest language features and improvements
    fix(Pipfile): add typing-extensions package to improve type hinting support

--- run.py
+++ run.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
-from distutils.util import strtobool
 from time import sleep
 
 import configargparse
 from github3 import login
 from github3.exceptions import NotFoundError, GitHubException
+
+
+def strtobool(val):
+    """
+    Convert a string representation of truth to True or False.
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'. Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return False
+    else:
+        raise ValueError(f"Invalid truth value {val}")
 
 
 def toggle_enforce_admin(options):
